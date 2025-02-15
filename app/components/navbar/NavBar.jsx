339303e5b3c4508b3c06ac08780f5e85c80ca2a1
simon <simon@mirinfosys.com>
2025-02-15 14:10:25 +0600

    navbar funtion added

--- app/components/navbar/NavBar.jsx
+++ app/components/navbar/NavBar.jsx
@@ -1,59 +1,119 @@
-import { Box, Container, Flex, Tabs, TabsList, Text } from '@chakra-ui/react';
-import React from 'react';
-import { Poppins } from "next/font/google"; 
+'use client'
+import { Box, Container, Flex, Tabs, TabsList, Text } from "@chakra-ui/react";
+import React, { act, useEffect, useState } from "react";
+import { Poppins } from "next/font/google";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
-// Load the Poppins font
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],  // Adding weights for Poppins
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Adding weights for Poppins
 });
 
-const navlist=[
-  {name:'Home', url:'/#'},
-  {name:'About', url:'/#'},
-  {name:'Projects', url:'/#'},
-  {name:'Contact', url:'/#'}
-]
 
-const NavBar = () => {
-  const landing = true;
+
+const NavBar = ({activeSection,navlist}) => {
+  const MotionText = motion(Text);
+  const [landing,setLanding] = useState(true);
+console.log(landing)
+useEffect(() => {
+  // Set a timeout to delay the state change by 1 second (1000ms)
+  const timer = setTimeout(() => {
+    if (activeSection.includes('home')) {
+      setLanding(true);
+    } else {
+      setLanding(false);
+    }
+  }, 100); // Adjust the delay as needed
+
+  // Cleanup function to clear the timer if the component unmounts before the timeout
+  return () => clearTimeout(timer);
+}, [activeSection]);
+
+const router = useRouter()
+const redirectUrl= (url)=> {
+  router.push(url)
+}
+
+  console.log(activeSection)
   return (
-    <Flex pt={{lg: '2vh'}} >
-       <Container
-      // Apply the font to the container using the Poppins CSS variable
-      className={poppins.className} 
-      sx={{ fontFamily: "var(--font-poppins), sans-serif" }}  // Fallback to sans-serif
-      display={'flex'}
-      alignItems={'center'}
-      bg={'#101010'} 
-      maxW={{lg: landing ? '42.92%' : '93.4%'}} 
-      h={{lg: '4.23vw'}} 
-      
-      p={'0.53vw'}
-      border={'0.066vw solid #232323'}
-      borderRadius={'5.29vw'}
-      boxShadow={'0px 0.93vw 2.01vw 0px rgba(0, 0, 0, 0.25)'}
+    <Flex
+      pt={{ lg: landing ? "2vh" : "3.061vw" }}
+      transition="padding-top 0.1s ease-in-out"
     >
-     <Tabs.Root defaultValue="members" variant="plain">
-      <Tabs.List  >
-       {navlist.map(data=>(
-         <Tabs.Trigger 
-         value={data?.name} 
-         color={'#FFFFFF'} fontSize={{lg:' 1.06vw'}} fontWeight={300} lineHeight={'24px'}
-         w={{md:'10.45vw'}} h={{md:' 3.17vw '}} justifyContent={'center'} roundedLeft={'full'}  roundedRight={'full'}
-         >
-        {data?.name}
-       </Tabs.Trigger>
-       ))}
-       
-        <Tabs.Indicator roundedLeft={'full'} border={'0.066vw solid #232323'}  roundedRight={'full'} bg={'#1D1D1D'}/>
-      </Tabs.List>
-     
-    </Tabs.Root>
-    </Container>
-    </Flex>
+      <Container
+        // Apply the font to the container using the Poppins CSS variable
+        className={poppins.className}
+        sx={{ fontFamily: "var(--font-poppins), sans-serif" }} // Fallback to sans-serif
+        display={"flex"}
+        alignItems={"center"}
+        bg={"#101010"}
+        maxW={{ lg: landing ? "43.35%" : "93.4%" }}
+        h={{ lg: "4.23vw" }}
+        p={"0.53vw"}
+        border={"0.066vw solid rgba(255, 255, 255, 0.06)"}
+        borderRadius={landing ? "5.29vw" : "1.058vw"}
+        boxShadow={"0px 0.93vw 2.01vw 0px rgba(0, 0, 0, 0.25)"}
+        transition="max-width 0.3s ease-in-out 0.1s"
+        justifyContent={"end"}
+      >
+        <MotionText
+      color="white"
+      flex={1}
+      initial={{ opacity: 0,display:'none' }}
+      animate={{ opacity: landing ? 0 : 1,     display:landing ? 'none':'block' }}
+      transition={{
+        opacity: { duration: 0.3, ease: "easeInOut", delay: 0.3 }, 
+        display: { duration: 0.3, ease: "easeInOut", delay: 0.3 },
    
+      }}
+     
+      // display={landing ? "none" : "block"}
+      w={{ md: "10.45vw" }}
+      // textAlign={'center'}
+      fontWeight={200}
+      fontSize={'1.587vw'}
+      ml={'2.381vw'}
+
+    >
+      S M Simon
+    </MotionText>
+        <Tabs.Root defaultValue="Home" variant="plain">
+          <Tabs.List>
+            {navlist.map((data, index) => (
+              <Tabs.Trigger
+              onClick={()=>redirectUrl(data.url)}
+              defaultValue={index[2]}
+              bg={'transparent'}
+                key={index}
+                value={data?.name}
+                color={"#FFFFFF"}
+                fontSize={{ lg: " 1.06vw" }}
+                fontWeight={200}
+                lineHeight={'1.587vw'}
+                w={{ md: "10.45vw" }}
+                h={{ md: " 3.17vw " }}
+                justifyContent={"center"}
+                roundedLeft={"full"}
+                roundedRight={"full"}
+              >
+                {data?.name}
+              </Tabs.Trigger>
+            ))}
+
+            <Tabs.Indicator
+              
+              border={"0.066vw solid #232323"}
+              rounded={landing ? '5.291vw':'12px'}
+              bg={"#1D1D1D"}
+              
+              
+            />
+          </Tabs.List>
+        </Tabs.Root>
+      </Container>
+    </Flex>
   );
 };
 
---
+++ app/components/pagewisecomponent/landingpage/HomePage.jsx
@@ -0,0 +1,104 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import MainSection from './MainSection';
+import About from './About';
+import { Box } from '@chakra-ui/react';
+import NavBar from '../../navbar/NavBar';
+import { usePathname, useSearchParams } from 'next/navigation'; // Import for the App Router
+
+
+const HomePage = ({navlist}) => {
+  const [activeSection, setActiveSection] = useState(navlist?.find(data => data.name)?.name.toLowerCase());
+  const pathname = usePathname(); // Get current path
+  const searchParams = useSearchParams(); // Get current query parameters
+
+  console.log('simon+++++++++++++++++', navlist);
+
+  
+  useEffect(() => {
+    let isScrolling = false;
+
+ 
+
+    const handleScroll = (event) => {
+      if (isScrolling) {
+        event.preventDefault(); 
+        return; 
+      }
+      isScrolling = true;
+
+      const mainSection = document.getElementById(navlist?.find(data => data.name.toLowerCase().includes('home'))?.name.toLowerCase());
+      const aboutSection = document.getElementById(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase());
+
+      if (event.deltaY > 0) { // scrolling down
+        if (window.scrollY >= mainSection.offsetTop + mainSection.offsetHeight - window.innerHeight) {
+          aboutSection.scrollIntoView({ behavior: "smooth", block: "start" });
+          setActiveSection(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase());
+        }
+      } else if (event.deltaY < 0) { // scrolling up
+        if (window.scrollY <= aboutSection.offsetTop) {
+          mainSection.scrollIntoView({ behavior: "smooth", block: "start" });
+          setActiveSection(navlist?.find(data => data.name.toLowerCase().includes('home'))?.name.toLowerCase());
+        }
+      }
+
+      setTimeout(() => {
+        isScrolling = false;
+      }, 600); 
+    };
+
+    const checkActiveSection = () => {
+      const mainSection = document.getElementById(navlist?.find(data => data.name.toLowerCase().includes('home'))?.name.toLowerCase());
+      const aboutSection = document.getElementById(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase());
+
+      const mainSectionOffset = mainSection.offsetTop;
+      const aboutSectionOffset = aboutSection.offsetTop;
+      
+      if (window.scrollY >= aboutSectionOffset - window.innerHeight / 2) {
+        setActiveSection(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase());
+      } else if (window.scrollY >= mainSectionOffset - window.innerHeight / 2) {
+        setActiveSection(navlist?.find(data => data.name.toLowerCase().includes('home'))?.name.toLowerCase());
+      }
+    };
+
+    // Check if the query param is `/?about` and scroll to the About section
+    const checkUrl = () => {
+      if (searchParams.has(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase())) {
+        const aboutSection = document.getElementById("about");
+        aboutSection?.scrollIntoView({ behavior: "smooth", block: "start" });
+        setActiveSection(navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase());
+      }
+    };
+
+    // Call the checkUrl function when the component mounts or pathname/searchParams change
+    checkUrl();
+
+    // Listen for changes in the pathname or search params
+    
+
+    window.addEventListener("wheel", handleScroll, { passive: false });
+    window.addEventListener("scroll", checkActiveSection);
+
+    return () => {
+      // Cleanup listeners
+      window.removeEventListener("wheel", handleScroll);
+      window.removeEventListener("scroll", checkActiveSection);
+    };
+  }, [pathname, searchParams]); // Watch for changes in pathname or search params
+
+  return (
+    <Box position={"relative"}>
+      <Box position={"fixed"} w={"100%"} zIndex={100}>
+        <NavBar activeSection={activeSection} navlist={navlist} />
+      </Box>
+      <Box id={navlist?.find(data => data.name.toLowerCase().includes('home'))?.name.toLowerCase()}>
+        <MainSection />
+      </Box>
+      <Box id={navlist?.find(data => data.name.toLowerCase().includes('about'))?.name.toLowerCase()}>
+        <About />
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
