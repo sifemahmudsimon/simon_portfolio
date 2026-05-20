c9d27e81951095567bce999ccc1cfeeefbbeba77
simon <simon@mirinfosys.com>
2026-05-20 15:23:15 +0600

    config fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,31 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+
   reactStrictMode: false,
+
   i18n: {
     locales: ["en", "bn"],
     defaultLocale: "en",
   },
+
   swcMinify: true,
+
+  turbopack: {},
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
+      use: ["@svgr/webpack"],
     });
 
     return config;
   },
+
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  
-
-  
 };
-
 
 export default nextConfig;
